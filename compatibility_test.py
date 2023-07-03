180423180b71f0b5cc2c32532e2be8eec8821b38
Daksh <daksh.rathore9@gmail.com>
2023-07-03 20:30:52 +0530

    made changes

--- compatibility_test.py
+++ compatibility_test.py
@@ -2,8 +2,10 @@
 This function is meant to be called from nwb_table_readme.py script to test NWBE compatibility
 '''
 import sys
-sys.path.insert(0, '/nwbe/nwb-explorer/nwb_explorer')
-from nwb_explorer.nwb_model_interpreter import NWBModelInterpreter, GeppettoModelAccess
+sys.path.insert(0, '/home/jovyan/nwb-explorer/nwb_explorer')
+import os
+print(os.getcwd())
+from nwb_model_interpreter import NWBModelInterpreter, GeppettoModelAccess
 
 def test_nwbe_compatibility(nwbfile_path):
 
